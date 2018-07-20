5546d48f7c1bb48221a8b5a6ca5de02733cd1b2d
jiulongteng <jiulongteng@myweimai.com>
2018-07-20 18:25:34 +0800

    bind-adapter

---
+++ bindadapter/src/main/java/com/ytjojo/databind/bindadapter/InverseBindingListener.java
@@ -0,0 +1,8 @@
+package com.ytjojo.databind.bindadapter;
+
+public interface InverseBindingListener {
+    /**
+     * Notifies the data binding system that the attribute value has changed.
+     */
+    void onChange();
+}
